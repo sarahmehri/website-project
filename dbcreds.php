663144102b2f3055e32431b508f9688a5b8fd98a
sarahmehri <72160953+sarahmehri@users.noreply.github.com>
2020-11-17 04:32:41 -0800

    Delete dbcreds.php

--- dbcreds.php
+++
@@ -1,8 +0,0 @@
-<?php
-//connecting to database
-$database = "grcacgre_grc";
-$username = "grcacgre_grcuser";
-$password = "Sarah@1995";
-$hostname = "localhost";
-$cnxn = @mysqli_connect($hostname, $username, $password, $database)
-or die("There was a problem.");
